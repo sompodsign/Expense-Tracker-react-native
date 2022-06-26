7075bf75ab7abf9ba9993ef2a7c65c144659079e
Shampad Sharkar <sompodsign@gmail.com>
2022-06-26 20:10:43 +0600

    deleted all files

--- src/axiosConfig.js
+++
@@ -1,3 +0,0 @@
-import axios from "axios";
-
-const axiosInstance = axios.create({ baseURL: 'http://192.168.31.38/api/' });
--- src/components/userAvatar.js
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-import { Avatar } from 'react-native-paper';
-
-const UserAvatar = () => (
-  <Avatar.Image size={40} source={require('../media/person.jpg')} />
-);
-export default UserAvatar
